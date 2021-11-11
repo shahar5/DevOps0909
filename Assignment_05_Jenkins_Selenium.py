57abb673d971b2ac86dd0d58dc1ef53cd1b6bd3c
shahar5 <shahar.reuven@gmail.com>
2021-11-11 18:12:39 +0200

    another commit

--- Assignment_05_Jenkins_Selenium.py
+++ Assignment_05_Jenkins_Selenium.py
@@ -3,3 +3,5 @@ from selenium import webdriver
 my_driver = webdriver.Chrome(executable_path="C:/Users/Shahar Reuven/Documents/שחר/Job/DevOps Expert/Lesson 4 - Testing/chromedriver.exe")
 website_url = "https://www.kan.org.il/"
 my_driver.get(website_url)
+
+print("shahar")
